a1ab3b4e401479ea7048af3afd508711696d38b2
stautisabela <stautisabela@gmail.com>
2023-01-03 11:49:48 +0100

    first commit

---
+++ simple-spring-calculator/src/main/java/com/stautisabela/simplespringcalculator/SimpleSpringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.stautisabela.simplespringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringCalculatorApplication.class, args);
+	}
+
+}
---
+++ simple-spring-calculator/src/test/java/com/stautisabela/simplespringcalculator/SimpleSpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stautisabela.simplespringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
